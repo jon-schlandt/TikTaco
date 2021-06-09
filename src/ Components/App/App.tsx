1d0666a11158b5ad771557315e9d111d630273f8
schlanjo <schlanjo@gmail.com>
2021-06-09 12:32:35 -0600

    Use effect in App to fetch random taco data

--- src/ Components/App/App.tsx
+++ src/ Components/App/App.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from 'react'
+
 import Header from '../Header/Header'
 import TacoGenerator from '../TacoGenerator/TacoGenerator'
 
+import { getTacoData } from '../../utils/apiCalls'
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    getTacoData()
+      .then(data => console.log(data))
+  })
+
   return (
     <div className="App">
       <Header />
--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -1,11 +1,8 @@
 export function getTacoData() {
-  fetch('http://taco-randomizer.herokuapp.com/random')
+  return fetch('http://taco-randomizer.herokuapp.com/random/')
     .then(resp => {
       if (resp.ok) {
         return resp.json()
       }
-    })
-    .then(data => {
-      console.log(data)
     })
 }
