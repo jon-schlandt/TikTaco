88c21372e704706bd4904f5b4184642a500e9cdc
schlanjo <schlanjo@gmail.com>
2021-06-09 17:59:21 -0600

    Implement taco generation on click, add state to TacoGenerator

--- src/ Components/App/App.tsx
+++ src/ Components/App/App.tsx
@@ -1,38 +1,14 @@
-import { useState, useEffect } from 'react'
-
 import Header from '../Header/Header'
 import TacoGenerator from '../TacoGenerator/TacoGenerator'
 
-import { getTacoData } from '../../utils/apiCalls'
 import './App.css';
 
-interface ITaco {
-  base: string,
-  mixin: string, 
-  condiment: string, 
-  seasoning: string,
-  shell: string
-}
-
 function App() {
-  const [taco, setTaco] = useState<ITaco>({
-    base: '',
-    mixin: '',
-    condiment: '',
-    seasoning: '',
-    shell: ''
-  })
-
-  useEffect(() => {
-    getTacoData()
-      .then(data => setTaco(data))
-  }, [])
-
   return (
     <div className="App">
       <Header />
       <main>
-        <TacoGenerator taco={taco}/>
+        <TacoGenerator />
       </main>
     </div>
   );
