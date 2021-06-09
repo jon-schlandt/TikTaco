be91b912a573db644bc2c422946c9f68bc9f4a9f
schlanjo <schlanjo@gmail.com>
2021-06-09 11:31:17 -0600

    Complete initial structure and design for taco display

--- src/ Components/TacoGenerator/TacoGenerator.tsx
+++ src/ Components/TacoGenerator/TacoGenerator.tsx
@@ -4,6 +4,9 @@ export default function TacoGenerator() {
   return (
     <div className='taco-generator'>
       <h1 className='greeting'>Good toppings!</h1>
+      <div className='taco-display'>
+        <p>Select the button below to generate a random taco.</p>
+      </div>
     </div>
   )
 }
