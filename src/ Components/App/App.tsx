6c614158cf0a19a78591bf0138d595d2aa4c71e6
schlanjo <schlanjo@gmail.com>
2021-06-10 14:18:50 -0600

    Add Taco-related state to App, add generateTaco() method

--- src/ Components/App/App.tsx
+++ src/ Components/App/App.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react'
 import { Route } from 'react-router-dom'
 
 import Header from '../Header/Header'
 import TacoGenerator from '../TacoGenerator/TacoGenerator'
 import TacoDetails from '../TacoDetails/TacoDetails'
 
+import { getTacoData } from '../../utils/apiCalls'
+import { ITaco } from '../../utils/types'
+
 import './App.css';
 
 function App() {
+  const [taco, setTaco] = useState<ITaco | null>(null)
+  const [error, setError] = useState('')
+
+  const generateTaco = () => {
+    getTacoData()
+      .then(data => setTaco(data))
+      .catch(error => setError(error.message))
+  }
+
   return (
     <div className="App">
       <Header />
