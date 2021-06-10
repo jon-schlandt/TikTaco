f552ab1bdcb95e8aeef7fb3e31184f41d3a14f73
schlanjo <schlanjo@gmail.com>
2021-06-10 09:56:28 -0600

    Add testing for taco generation when Generate Taco is clicked

--- cypress/integration/generator.spec.js
+++ cypress/integration/generator.spec.js
@@ -1,5 +1,3 @@
-export {}
-
 describe('TacoGenerator on first load', () => {
   beforeEach('set up tests', () => {
     cy.visit('http://localhost:3000')
@@ -14,25 +12,33 @@ describe('TacoGenerator on first load', () => {
   it('should display an informative message in the taco display', () => {
     cy.get('.taco-generator')
       .find('.taco-display').should('be.visible')
-      .contains('Select the button below to generate a random taco.')
+      .find('p').contains('Select the button below to generate a random taco.')
   })
 
-  it('should only display a button for generating a random taco', () => {
+  it('should display a button for generating a random taco', () => {
     cy.get('.taco-generator')
-      .find('button').should('be.visible')
+      .find('.primary-btn').should('be.visible')
       .contains('Generate Taco')
   })
 })
 
 describe('Generate Taco button', () => {
   beforeEach('set up tests', () => {
-    cy.addTacoIntercept()
+    cy.setTacoIntercept()
     cy.visit('http://localhost:3000')
   })
 
-  it('should generate a random taco that displays in the taco display', () => {
-    cy.get('button').click()
+  it('should generate a random taco that displays in the taco display when clicked', () => {
+    cy.get('.primary-btn').click()
 
     cy.get('.taco-display')
+      .find('p').contains('Baked Tilapia with Black Olives, ganished with Lettuce (Traditional; US) topped off with Mahi Mahi Rub and wrapped in a delicious Fresh Corn Tortillas')
+  })
+
+  it('should update its inner text to \'View Details when clicked', () => {
+    cy.get('.primary-btn').eq(0).click()
+
+    cy.get('.primary-btn').should('be.visible')
+      .contains('View Details')
   })
 })
