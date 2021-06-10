869e32c0b00af11be5350eed2ac2f2eb8178df22
schlanjo <schlanjo@gmail.com>
2021-06-10 00:00:50 -0600

    Set taco intercept custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('setTacoIntercept', () => {
+  cy.fixture('../fixtures/taco.json')
+    .then(taco => cy.intercept('http://taco-randomizer.herokuapp.com/random/', taco))
+})
