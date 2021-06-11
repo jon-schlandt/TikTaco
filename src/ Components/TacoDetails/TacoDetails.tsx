1ab1500004c605db6287149aaf6fde07d1b3d7de
schlanjo <schlanjo@gmail.com>
2021-06-11 10:23:14 -0600

    Render recipe list and links

--- src/ Components/TacoDetails/TacoDetails.tsx
+++ src/ Components/TacoDetails/TacoDetails.tsx
@@ -14,6 +14,14 @@ export default function TacoDetails({tacoDetails}: IProps) {
         <img src={tacoDetails.image} alt='image of taco' />
       </div>
       {displayText}
+      <ul className='recipe-list'>
+        <h1>Recipes</h1>
+        <li><a href={tacoDetails.base_layer.url}>{tacoDetails.base_layer.name}</a></li>
+        <li><a href={tacoDetails.mixin.url}>{tacoDetails.mixin.name}</a></li>
+        <li><a href={tacoDetails.condiment.url}>{tacoDetails.condiment.name}</a></li>
+        <li><a href={tacoDetails.seasoning.url}>{tacoDetails.seasoning.name}</a></li>
+        <li><a href={tacoDetails.shell.url}>{tacoDetails.shell.name}</a></li>
+      </ul>
     </div>
   )
 }
