6049f494ac148ea0aaee0c054bf40852169a7eea
schlanjo <schlanjo@gmail.com>
2021-06-11 13:06:05 -0600

    Test for taco image rendering

--- cypress/integration/details.spec.js
+++ cypress/integration/details.spec.js
@@ -4,13 +4,23 @@ describe('\'View Details\' button', () => {
   })
 
   it('should render the TacoDetails page when clicked', () => {
-    cy.get('.taco-generator > a').contains('View Details').click()
+    cy.get('.taco-generator > a').click()
       .url().should('eq', 'http://localhost:3000/details')
   })
 })
 
 describe('TacoDetails on render', () => {
-  
+  beforeEach('set up tests', () => {
+    cy.generateTaco()
+    cy.get('.taco-generator > a').click()
+  })
 
-  // it('should display a sel')
+  it('should display an image of a taco', () => {
+    cy.fixture('../fixtures/taco-image.json')
+      .then(image => {
+        cy.get('.taco-image > img')
+          .should('have.attr', 'src', image.urls.regular)
+          .should('have.attr', 'alt', 'Baked Tilapia taco')
+      })
+  })
 })
