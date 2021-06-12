753b9399c88036a2a6f38e0411b084f1b1e92571
schlanjo <schlanjo@gmail.com>
2021-06-12 17:21:12 -0600

    Test for details page rendering for favorited taco

--- cypress/integration/favorites.spec.js
+++ cypress/integration/favorites.spec.js
@@ -38,8 +38,25 @@ describe('Favorites view on render', () => {
       .contains('Baked Tilapia with Black Olives, ganished with Lettuce (Traditional; US) topped off with Mahi Mahi Rub and wrapped in a delicious Fresh Corn Tortillas')
   })
 
-  describe('Favorited taco on Favorites view', () => {
+  describe('Favorited taco within Favorites view', () => {
+    beforeEach('set up tests', () => {
+      cy.generateAndFavoriteTaco()
+    })
+    
+    it('should render the favorited taco\'s details view when clicked', () => {
+      cy.get('.display-text').click()
+        .url().should('eq', 'http://localhost:3000/details/LBBFM')
 
+      cy.get('.taco-image').should('be.visible')
+        .find('img')
+        .should('have.attr', 'src', 'https://images.unsplash.com/photo-1598902289654-6c1ba478eaa0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzg2NDJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjM0MzUwNjc&ixlib=rb-1.2.1&q=80&w=1080')
+
+      cy.get('.display-text').should('be.visible')
+        .contains('Baked Tilapia with Black Olives, ganished with Lettuce (Traditional; US) topped off with Mahi Mahi Rub and wrapped in a delicious Fresh Corn Tortillas')
+
+      cy.get('.recipe-list').should('be.visible')
+        .find('a').should('have.length', 5)
+    })
   })
 
   afterEach('clear session storage', () => {
