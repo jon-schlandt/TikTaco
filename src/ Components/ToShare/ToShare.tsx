e8c2bd214ab991f24a8268d74e62e82c14e75d55
schlanjo <schlanjo@gmail.com>
2021-06-11 19:37:17 -0600

    Add title, set initial styling for title and display

--- src/ Components/ToShare/ToShare.tsx
+++ src/ Components/ToShare/ToShare.tsx
@@ -1,19 +1,24 @@
+import { Link } from 'react-router-dom'
+
 import { formatDisplayText, IShapedTacoDetails } from "../../utils/utilites"
+import './ToShare.css'
 
 export default function ToShare({favorites}: {favorites: IShapedTacoDetails[]}) {
   const favoriteDisplays = favorites.map(favorite => {
     return (
-      <div 
+      <Link
+        to={`to-share${favorite.id}`} 
         key={favorite.id}
         className='favorite-display'
       >
         {formatDisplayText(favorite)}
-      </div>
+      </Link>
     )
   })
 
   return (
     <div className='no-share'>
+      <h1 className='to-share-title'>Tacos to Share</h1>
       {favoriteDisplays}
     </div>
   )
