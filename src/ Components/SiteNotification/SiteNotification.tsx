519b764fea151573a0b1daab32111c257aa6ba66
schlanjo <schlanjo@gmail.com>
2021-08-30 20:39:00 -0600

    Set positioning/sizing for notification model

--- src/ Components/SiteNotification/SiteNotification.tsx
+++ src/ Components/SiteNotification/SiteNotification.tsx
@@ -3,7 +3,7 @@ import './SiteNotification.css'
 export default function SiteNotification() {
   return (
     <div className='sn-overlay'>
-      
+      <div className='site-notification'></div>
     </div>
   )
 }
